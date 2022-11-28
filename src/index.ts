2edf5be26b4a8ba8548d41e43db9448e607f5629
Atlantik <atlantiklimani1@gmail.com>
2022-11-28 20:42:58 +0100

    Get individual sheets

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import { TelefoniaFixeController } from "./controllers/TelefoniaFixe.controller";
 import { TeHyratDheInvestimetISPController } from "./controllers/TeHyratDheInvestimetISP.controller";
 import { TelefoniaMobileController } from "./controllers/TelefoniaMobile.controller";
+import { ReadFileData } from "./utils/readFileData";
 
 // Boot express
 const app: Application = express();
@@ -16,5 +17,20 @@ app.use("/ping", (req: Request, res: Response, next: NextFunction) => {
 app.use("/telefonia-fixe", TelefoniaFixeController);
 app.use("/telefonia-mobile", TelefoniaMobileController);
 app.use("/invesimet-isp", TeHyratDheInvestimetISPController);
+// app.get("/api/investimet/:sheetNo",TeHyratSheetController);
+app.get("/api/investimet/:sheetNo", (req,res) => {
+    let d = ReadFileData("TeHyratDheInvestimetISP.xlsx");
+     
+    let sheetNo = req.params.sheetNo;
+    for( let sheet of d){
+      if(sheet.name.replace(/ /g,'') == sheetNo){
+        return res.send(sheet);
+      }
+    }
+    res.send('Not found');
+    
+    //Idea: Njejte si ne file per telefoni fixe   
+})
+
 // Start server
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
