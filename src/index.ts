656b1533bc18091e12dad2663cf7d639fb5fbc88
Atlantik <atlantiklimani1@gmail.com>
2023-01-06 12:50:24 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,16 @@ app.use("/ping", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: "Hello from server" });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/telefonia-fixe", TelefoniaFixeController);
 app.use("/telefonia-mobile", TelefoniaMobileController);
 app.use("/investimet-isp", TeHyratDheInvestimetISPController);
+
 
 
 // Start server
