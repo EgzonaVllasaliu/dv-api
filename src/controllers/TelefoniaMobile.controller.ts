0905897f005ec2554461d16ccb38911886e969dd
Puhizë Doçi <puhizad@gmail.com>
2023-01-08 23:23:40 +0100

    Endpoint-telefonia mobile.

--- src/controllers/TelefoniaMobile.controller.ts
+++ src/controllers/TelefoniaMobile.controller.ts
@@ -4,31 +4,12 @@ import { TelefoniaMobileService } from "../services/TelefoniaMobile.service";
 export const TelefoniaMobileController: Router = Router();
 
 TelefoniaMobileController.get(
-  "/",
+  "/api/sheet/:sheetName",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const result = await TelefoniaMobileService.read_all_data();
-
-      res.status(result.status).send({
-        data: result.data,
-      });
-    } catch (e) {
-      next(e);
-    }
-  }
-);
-
-TelefoniaMobileController.get(
-  "/api/Ndarja_e_tregut/:operator_name",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      let {operator_name} =  req.params;
-      console.log('Operator Name: ',operator_name)
-      const result = await TelefoniaMobileService.get_market_division(operator_name);
-
-      res.status(result.status).send({
-        data: result.data,
-      });
+      const {sheetName} = req.params;
+      let result = await TelefoniaMobileService.read_one_sheet(sheetName);
+      res.status(result.status).send(result)
     } catch (e) {
       next(e);
     }
--- src/services/TelefoniaMobile.service.ts
+++ src/services/TelefoniaMobile.service.ts
@@ -1,57 +1,16 @@
 import { ReadFileData } from "../utils/readFileData";
 
 export const TelefoniaMobileService = {
-  read_all_data: async () => {
-    const data = ReadFileData("TelefoniaMobile.xlsx");
-
-    //Idea: Njejte si ne file paraprak
-
-    return { status: 200, data };
-  },
-  get_market_division : async (operator_name : String) => {
-    const data = ReadFileData("TelefoniaMobile.xlsx");
-
-    for(let sheet of data){
-      if(sheet.name.toLowerCase() == "ndarja e tregut sipas te hyrave"){
-
-        let quarters = sheet.data[1] as String[]
-        let year = 2007;
-        let rez : Object[]= []; 
-        for(let row of sheet.data as String[]){
-          // if(typeof row[0] !== "undefined"){
-
-            if(typeof row[0] !== 'undefined' &&  row[0].toLowerCase().trim() == operator_name.toLowerCase()){
-              
-              for(let i = 1; i < row.length; i++){
-                if(typeof quarters[i] !== 'undefined' &&  quarters[i] != null && quarters[i] != ''){
-                  rez.push({
-                    "year": year,
-                    "quarter": quarters[i],
-                    "value": row[i]  
-                  })
-
-                  if(quarters[i].toLowerCase().trim() == "tm4"){
-                    year = year + 1;
-                  }
-                }
-              }
-              return {
-                status : 200,
-                data: rez
-              } 
-            }  
-          // }
-            
-
-          
-        }
-       
+  read_one_sheet : async (sheetName: string) =>{
+    let d = ReadFileData("TelefoniaMobileRefactored.xlsx");
+    
+    for(let sheet of d){
+      console.log(sheet.name)
+      if(sheet.name.replace(/ /g,'') == sheetName){
+        return {status:200,sheet};
       }
     }
 
-    return {
-      status : 200,
-      data: []
-    }
+    return {status:400, data: []};
   }
 };
