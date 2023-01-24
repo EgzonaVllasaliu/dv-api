9a6630116afe097a8f118a18848ad97b1e62f886
Puhizë Doçi <puhizad@gmail.com>
2023-01-24 16:13:40 +0100

    Added endpoint to get years for listing

--- src/services/TelefoniaMobile.service.ts
+++ src/services/TelefoniaMobile.service.ts
@@ -11,5 +11,26 @@ export const TelefoniaMobileService = {
     }
 
     return {status:400, data: []};
+  },
+
+  getYears : () => {
+    let data = ReadFileData("TelefoniaMobileRefactored.xlsx");
+    let result : string[] = [];
+
+    for (let sheet of data){
+      let time = sheet.data[0] as Array<string>;
+
+      time.forEach(elem => {
+        if(!result.includes(elem.toString().split(' ')[1])){
+          result.push(elem.toString().split(' ')[1]);  
+        }
+      })
+    }
+
+    return {
+      status: 200,
+      data: result.sort().reverse()
+    };
+
   }
 };
