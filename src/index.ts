c36cd5e1f359065c0c71b1b6f49626c929776fe8
Egzona Vllasaliu <egzona.vllasaliu@studentet.uni-pr.edu>
2023-01-24 16:23:30 +0100

    Add vercel

--- src/index.ts
+++ src/index.ts
@@ -14,17 +14,18 @@ app.use("/ping", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: "Hello from server" });
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 app.use("/telefonia-fixe", TelefoniaFixeController);
 app.use("/telefonia-mobile", TelefoniaMobileController);
 app.use("/investimet-isp", TeHyratDheInvestimetISPController);
-
-
 
 // Start server
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
